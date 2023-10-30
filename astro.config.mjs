bb8a8084582c13d6ccc9c950cc3a796f8c81b1fd
kyeshmz <kyeshimizu@gmail.com>
2023-10-30 22:02:55 +0900

    add bun lock files for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), prefetch(), sitemap(), tailwind(), partytown()]
+});
