81b37748c082e02c7fdaad4a78f16a4dc45f6bb8
kyeshmz <kyeshimizu@gmail.com>
2023-10-30 23:02:32 +0900

    remove party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
-import prefetch from "@astrojs/prefetch";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import prefetch from '@astrojs/prefetch'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), prefetch(), sitemap(), tailwind(), partytown()]
-});
+  integrations: [preact(), prefetch(), sitemap(), tailwind()],
+})
