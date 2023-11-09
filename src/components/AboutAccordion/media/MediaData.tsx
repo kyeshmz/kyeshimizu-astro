78f1508e9c6369064d54e7383aa95d2f4d6dfa05
kyeshmz <kyeshimizu@gmail.com>
2023-11-09 17:32:35 +0900

    add media, but jesus

---
+++ src/components/AboutAccordion/media/MediaData.tsx
@@ -0,0 +1,30 @@
+import type { MediaTableRow } from './MediaTable'
+import { Works } from '@/src/types/works'
+
+export const MediaData: MediaTableRow[] = [
+  {
+    work: Works.algorithmic_couture,
+    media_name: 'H&M',
+
+    reference: 'https://about.hm.com/ja_jp/news/general-news-2019/_.html',
+    year: 2019,
+  },
+  {
+    work: Works.algorithmic_couture,
+    media_name: 'AXIS',
+    reference: 'https://www.axismag.jp/posts/2019/04/124419.html',
+    year: 2019,
+  },
+  {
+    work: Works.algorithmic_couture,
+    media_name: 'H&M',
+    reference: 'https://prtimes.jp/main/html/rd/p/000000283.000011958.html',
+    year: 2019,
+  },
+  {
+    work: Works.algorithmic_couture,
+    media_name: 'Senken Newspaper',
+    reference: 'https://senken.co.jp/posts/toyoshima-200108',
+    year: 2019,
+  },
+]
---
+++ src/components/AboutAccordion/media/MediaTable.tsx
@@ -0,0 +1,8 @@
+import { type WorkType } from '@/src/types/works'
+export type MediaTableRow = {
+  work: WorkType
+  media_name: string
+
+  reference: string
+  year: number
+}
