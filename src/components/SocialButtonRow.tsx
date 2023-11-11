45645d9c5e977b3211d3044c4e839827d0dd5df0
kyeshmz <kyeshimizu@gmail.com>
2023-11-11 10:46:38 +0900

    Add social media icons and separator to SocialButtonRow component

--- src/components/SocialButtonRow.tsx
+++ src/components/SocialButtonRow.tsx
@@ -1,29 +1,34 @@
 import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 import MailButton from './MailButton'
 import { Button } from './ui/button'
+import { Separator } from './ui/separator'
 
 export default function SocialButtonRow() {
   return (
-    <div className='flex justify-between w-full'>
-      <a href='https://github.com/kyeshmz'>
-        <Button variant='outline' className='flex justify-between'>
-          <GitHubLogoIcon />
-          <p>@kyeshmz</p>
-        </Button>
-      </a>
-      <a href='https://twitter.com/kyeshimizu'>
-        <Button variant='outline' className='flex justify-between'>
-          <TwitterLogoIcon />
-          <p>@kyeshimizu</p>
-        </Button>
-      </a>
-      <a href='https://linkedin.com/in/kye-shimizu-363805a8'>
-        <Button variant='outline' className='flex justify-between'>
-          <LinkedInLogoIcon />
-          <p>@kyeshmz</p>
-        </Button>
-      </a>
-      <MailButton />
+    <div className='flex flex-col'>
+      <p>Socials</p>
+      <Separator />
+      <div className='flex justify-between w-full'>
+        <a href='https://github.com/kyeshmz'>
+          <Button variant='outline' className='flex justify-between'>
+            <GitHubLogoIcon />
+            <p>@kyeshmz</p>
+          </Button>
+        </a>
+        <a href='https://twitter.com/kyeshimizu'>
+          <Button variant='outline' className='flex justify-between'>
+            <TwitterLogoIcon />
+            <p>@kyeshimizu</p>
+          </Button>
+        </a>
+        <a href='https://linkedin.com/in/kye-shimizu-363805a8'>
+          <Button variant='outline' className='flex justify-between'>
+            <LinkedInLogoIcon />
+            <p>@kyeshimizu</p>
+          </Button>
+        </a>
+        <MailButton />
+      </div>
     </div>
   )
 }
