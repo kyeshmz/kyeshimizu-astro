b2fba438dfcd8e975c5f00a6e645bb07b526de41
kye shimizu <kyeshimizu@gmail.com>
2023-11-13 15:17:24 +0900

    Update day.ts

--- src/lib/day.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-import timezone from 'dayjs/plugin/timezone'
-import utc from 'dayjs/plugin/utc'
-
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.tz.setDefault('Asia/Tokyo')
-export default dayjs
