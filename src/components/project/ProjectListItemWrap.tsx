f3320e9821e53173c134f74d21a28482cfb4e361
kyeshmz <kyeshimizu@gmail.com>
2023-11-19 08:48:19 +0900

    Update ProjectListItemWrap component to include title prop

--- src/components/project/ProjectListItemWrap.tsx
+++ src/components/project/ProjectListItemWrap.tsx
@@ -1,7 +1,14 @@
 import { Suspense } from 'react'
 import { $projectHoverImageAtom } from '../../state/ProjectHoverState'
+import type { WorkType } from '../../types/works'
 
-export default function ProjectListItemWrap({ children }: { children: React.ReactNode }) {
+export default function ProjectListItemWrap({
+  children,
+  title,
+}: {
+  children: React.ReactNode
+  title: WorkType | null
+}) {
   return (
     <Suspense
       fallback={<li className={'flex flex-col w-6/12 max-w-[160px]  flex-wrap '}>{children}</li>}
