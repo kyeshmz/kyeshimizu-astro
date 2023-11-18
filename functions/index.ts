577c94f50b4189909811f5504c9ec57e83fef289
kyeshmz <kyeshimizu@gmail.com>
2023-11-19 00:10:28 +0900

    remove functions

--- functions/index.ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  const url = new URL(context.request.url)
-
-  return fetch(`https://cf-cloudinary-image.kyeshimizu.workers.dev${url.pathname}`, {
-    headers: context.request.headers,
-    body: context.request.body,
-    method: context.request.method,
-  })
-}
--- functions/test.ts
+++
@@ -1,3 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  return new Response('Hello, world!')
-}
