58999e9bb907ff7860b13508ba557b32035dcb5e
kyeshmz <kyeshimizu@gmail.com>
2023-11-24 11:33:51 +0900

    Update dependencies and add SideBar component

--- src/components/layout/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Sidebar() {
-  return (
-    <div className={'float-left w-100 relative hidden md:block'}>
-      <span className={'fixed bottom-6 right-8 -rotate-90 whitespace-nowrap text-xs w-0'}>
-        ( c ) 2015 - 2023
-      </span>
-    </div>
-  )
-}
