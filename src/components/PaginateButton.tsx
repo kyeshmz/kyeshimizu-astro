0066ed0470feeea687aa220e5604545b1af4f6cc
kyeshmz <kyeshimizu@gmail.com>
2023-11-27 22:51:06 +0900

    Refactor code formatting in PaginateButton and BaseLayout components

--- src/components/PaginateButton.tsx
+++ src/components/PaginateButton.tsx
@@ -1,7 +1,9 @@
-export default function PaginateButton({url}:{url: string}){
- return <div>
-<a href={url}> </a>
-</div>
+export default function PaginateButton({ url }: { url: string }) {
+  return (
+    <div>
+      <a href={url}> </a>
+    </div>
+  )
 }
 
 //https://rimdev.io/creating-a-pagination-component-with-astro
