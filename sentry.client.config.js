aa8fb98189bc72a60988a37d2ed623c682532c36
kyeshmz <kyeshimizu@gmail.com>
2023-12-08 10:56:46 +0900

    Add Astro Lighthouse plugin and Photoswipe library

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,10 +4,12 @@ sentry.init({
   dsn: 'https://740dd5065bf6f581619af5cb14f71e94@o4505485115719680.ingest.sentry.io/4506272625065984',
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  integrations: [
-    new sentry.Replay({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
+  integrations: [],
 })
+
+sentry.addIntegration(
+  new sentry.Replay({
+    maskAllText: false,
+    blockAllMedia: false,
+  }),
+)
